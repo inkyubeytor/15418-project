ae2258719c43d02e165b44a7561039de5752a00a
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2022-04-09 22:42:52 -0400

    parameterized graph with cost type

--- lib/graph.cpp
+++ lib/graph.cpp
@@ -8,46 +8,46 @@ using std::stack;
 using std::reverse;
 using std::vector;
 
-class Graph {
+template <class T> class Graph {
 private:
     vector <vector<int>> adj_list;
-    vector<float> weights;
+    vector<T> weights;
 public:
-    Graph(vector <vector<int>> input_adj_list, vector<float> input_weights)
-            : adj_list(input_adj_list), weights(input_weights) {
-        assert(input_adj_list.size() == weights.size());
+    Graph(vector <vector<int>> adj_list, vector<T> weights)
+            : adj_list(adj_list), weights(weights) {
+        assert(adj_list.size() == weights.size());
     }
 
-    int size() { return weights.size(); }
+    int size() { return this->weights.size(); }
 
-    float weight(int node) { return weights[node]; }
+    float weight(int node) { return this->weights[node]; }
 
-    vector<float> copy_weights() {
-        vector<float> weights_copy(weights);
+    vector<T> copy_weights() {
+        vector<T> weights_copy(this->weights);
         return weights_copy;
     }
 
-    const vector<int> &neighbors(int node) { return adj_list[node]; }
+    const vector<int> &neighbors(int node) { return this->adj_list[node]; }
 };
 
-class Tree : public Graph {
+template <class T> class Tree : public Graph<T> {
 private:
     int root;
 public:
-    Tree(vector <vector<int>> input_adj_list, vector<float> input_weights,
-         int root_node) : Graph(input_adj_list, input_weights),
+    Tree(vector <vector<int>> adj_list, vector<T> weights,
+         int root_node) : Graph<T>(adj_list, weights),
                           root(root_node) {}
 
     vector<int> dfs_postorder_nodes() {
         vector<int> traversal;
-        traversal.reserve(size());
+        traversal.reserve(this->size());
         stack<int> stack;
-        stack.push(root);
+        stack.push(this->root);
         while (!stack.empty()) {
             int curr = stack.top();
             stack.pop();
             traversal.push_back(curr);
-            for (const int& child: neighbors(curr)) stack.push(child);
+            for (const int& child: this->neighbors(curr)) stack.push(child);
         }
         reverse(traversal.begin(), traversal.end());
         return traversal;
@@ -61,7 +61,7 @@ int main() {
     vector<int> n2 = {3};
     vector<int> n3 = {};
     vector <vector<int>> adj = {n0, n1, n2, n3};
-    Tree T(adj, weights, 0);
+    Tree<float> T(adj, weights, 0);
     vector<int> traversal = T.dfs_postorder_nodes();
     for (int x: traversal) std::cout << x << " ";
     std::cout << std::endl;
